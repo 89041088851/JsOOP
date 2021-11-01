49454575e36c06a50c03e614e7956acf3192d0f7
89041088851 <bogdan.kosnyrev@outlook.com>
2021-11-01 17:35:16 +0300

    first commit

---
+++ CarOOP1/js/startCarProccess.js
@@ -0,0 +1,113 @@
+function startCarProccess(containerId) {
+
+  function render() {
+    var container = document.getElementById(containerId);
+    container.innerHTML = `
+    <div class="car"
+      <div class="info-panel">
+        <label>status: </label>
+        <span data-role="status">off</span>
+      </div>
+      <div class="controls">
+        <input data-role="start-car" type="button" value="Start">
+        <hr>
+        <label>Gear box: </label>
+        <span data-role="gear-box-value">N</span>
+      </div>
+    </div>
+    `;
+  }
+
+  function carStartListener() {
+    var randomNumber = Math.random();
+
+    if (randomNumber > 0.5) {
+      carStarted();
+    } else {
+      carCannotBeStarted();
+    }
+  }
+
+  function drawStatus(status) {
+    processEls(statusLabels, function (item) {
+      item.innerHTML = status;
+    });
+  }
+
+  function processEls(arrayOfEls, proccessor) {
+    for (var i = 0; i < arrayOfEls.length; i++) {
+      var item = arrayOfEls[i];
+      proccessor(item);
+    }
+  }
+
+
+  function devLog(message) {
+    console.log(message);
+  }
+
+  function carStarted() {
+    drawStatus('Car have started');
+    devLog('Car have started');
+
+    processEls(startButtons, function (startButton) {
+      startButton.classList.add('hide');
+    });
+
+    gearBoxStarted();
+    plannedCrashStarted();
+    devLog('we wait crash');
+  }
+
+  function carCannotBeStarted() {
+    devLog('Something wrong');
+    drawStatus('Car can`t be starteed. Try again!');
+  }
+
+  function gearBoxStarted() {
+    var gearBoxValue = 1;
+
+    processEls(gearBoxValueLabels, function (gearBoxValueLabel) {
+      gearBoxValueLabel.innerHTML = gearBoxValue;
+    });
+
+    function incrementGearBoxValue() {
+      if (gearBoxValue < 5) {
+        gearBoxValue++;
+        processEls(gearBoxValueLabels, function (gearBoxValueLabel) {
+          gearBoxValueLabel.innerHTML = gearBoxValue;
+        });
+      }
+    }
+    gearBoxInterval = window.setInterval(incrementGearBoxValue, 1000);
+  }
+
+  function plannedCrashStarted() {
+    function engineCrashed() {
+      devLog('engine crashed');
+      drawStatus('Engine have crashed. Car have stopped');
+
+      processEls(startButtons, function (startButton) {
+        startButton.classList.remove('hide');
+      });
+
+      processEls(gearBoxValueLabels, function (gearBoxValueLabel) {
+        gearBoxValueLabel.innerHTML = 'N';
+      });
+
+      window.clearInterval(gearBoxInterval);
+    }
+    window.setTimeout(engineCrashed, 3000);
+  }
+
+  render();
+
+  var gearBoxInterval;
+  var startButtons = document.getElementById(containerId).querySelectorAll('[data-role="start-car"]');
+  var statusLabels = document.getElementById(containerId).querySelectorAll('[data-role="status"]');
+  var gearBoxValueLabels = document.getElementById(containerId).querySelectorAll('[data-role="gear-box-value"]');
+
+  processEls(startButtons, function (startButton) {
+    startButton.addEventListener('click', carStartListener);
+  });
+}
